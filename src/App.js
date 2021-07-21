2578f4e87e83e2b0bfb97ea8d0eee1bc887e2d25
Anders Strömberg <40022844+anst9000@users.noreply.github.com>
2021-07-21 15:46:45 +0200

    First part of React useReducer hook A simple app using two buttons to increment or decrement count value.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import "./styles.css";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement"
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 };
+
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 };
+
+    default:
+      return state;
+  }
+};
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  function increment() {
+    dispatch({ type: ACTIONS.INCREMENT });
+  }
+
+  function decrement() {
+    dispatch({ type: ACTIONS.DECREMENT });
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
   );
 }
